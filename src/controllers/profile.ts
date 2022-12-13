f1881bc55512007ff3d01cd466fbaac503151ea0
SHY <ekdmbzzang@naver.com>
2022-12-13 14:38:25 +0900

    [FEAT] Profile Detail Controller

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -5,7 +5,7 @@ import * as profileService from '@/services/profile';
 export const create = async (
   req: Request<Record<string, never>, Record<string, never>, profileDto>,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   try {
     const profileData = req.body;
@@ -14,5 +14,21 @@ export const create = async (
     res.status(200).send({ status: 200, data: null });
   } catch (err) {
     next(err);
+  }
+};
+
+export const view = async (
+  req: Request<Record<string, never>, Record<string, never>>,
+  res: Response,
+  next: NextFunction
+) => {
+  /*
+    View Profile Detail Controller
+  */
+  try {
+    const result = await profileService.view(req);
+    res.status(200).send({ status: 200, data: result });
+  } catch (err) {
+    res.status(err.status).send({ status: err.status, message: err.message });
   }
 };
