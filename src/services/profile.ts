0e031886b0e0815a5471dc4258ef3da1a09277fa
SHY <ekdmbzzang@naver.com>
2022-12-13 14:40:41 +0900

    [FEAT] Profile Detail Service

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -2,18 +2,17 @@ import { Request } from 'express';
 import ProfileGuest from '@/entities/profile_guest';
 import ProfileHostGithub from '@/entities/profile_host_github';
 import ProfileHostTag from '@/entities/profile_host_tag';
-import { fileDto, profileDto } from '@/interfaces/profile';
+import { fileDto, profileDetailDto, profileDto } from '@/interfaces/profile';
 import AppDataSource from '@/config/app-data-source';
+import { nextTick } from 'process';
+import { profile } from 'console';
 
 export const create = async (req: Request, profileData: profileDto, fileData: fileDto) => {
-  const {
-    name, username, bio, type, github, stack,
-  } = profileData;
+  const { name, username, bio, type, github, stack } = profileData;
 
   // TODO - upload profile image file to AWS S3 using fileData
 
-  const newProfile = await AppDataSource
-    .createQueryBuilder()
+  const newProfile = await AppDataSource.createQueryBuilder()
     .insert()
     .into(ProfileGuest)
     .values({
@@ -29,8 +28,7 @@ export const create = async (req: Request, profileData: profileDto, fileData: fi
   const { profileGuestId } = newProfile.raw as { profileGuestId: number };
 
   if (type === 'host') {
-    await AppDataSource
-      .createQueryBuilder()
+    await AppDataSource.createQueryBuilder()
       .insert()
       .into(ProfileHostGithub)
       .values({
@@ -39,18 +37,90 @@ export const create = async (req: Request, profileData: profileDto, fileData: fi
       })
       .execute();
 
-    const promises = stack.map((tag) => AppDataSource
-      .createQueryBuilder()
-      .insert()
-      .into(ProfileHostTag)
-      .values({
-        profileGuestId,
-        tag,
-      })
-      .execute());
+    const promises = stack.map((tag) =>
+      AppDataSource.createQueryBuilder()
+        .insert()
+        .into(ProfileHostTag)
+        .values({
+          profileGuestId,
+          tag,
+        })
+        .execute()
+    );
 
     await Promise.all(promises);
   }
 
   return true;
+};
+
+export const view = async (req: Request) => {
+  /*
+    View Profile Detail Service
+  */
+
+  let profileDetail: profileDetailDto = {
+    isHost: false,
+    isOwner: false,
+    id: 0,
+    name: '',
+    username: '',
+    bio: '',
+    github: '',
+    stack: [],
+  };
+
+  try {
+    const profileGuestObj = await AppDataSource.getRepository(ProfileGuest)
+      .createQueryBuilder('profile_guest')
+      .where('profile_guest.id = :id', { id: req.params.id })
+      .getOneOrFail();
+
+    profileDetail = {
+      ...profileDetail,
+      ...profileGuestObj,
+    };
+  } catch (err) {
+    const error = new Error(err.message);
+    (error as any).status = 400;
+    throw error;
+  }
+
+  try {
+    const profileHostGithub = await AppDataSource.getRepository(ProfileHostGithub)
+      .createQueryBuilder('profile_host_github')
+      .where('profile_host_github.profile_guest_id = :id', { id: profileDetail.id })
+      .select(['profile_host_github.github'])
+      .getOne();
+
+    const isHost = profileHostGithub !== null;
+
+    if (isHost) {
+      const profileHostTags = await AppDataSource.getRepository(ProfileHostTag)
+        .createQueryBuilder('profile_host_tag')
+        .where('profile_host_tag.profile_guest_id = :id', { id: profileDetail.id })
+        .select(['profile_host_tag.tag'])
+        .getMany();
+
+      profileDetail = {
+        ...profileDetail,
+        ...profileHostGithub,
+        isHost,
+        stack: profileHostTags.map((ProfileHostTag) => ProfileHostTag.tag),
+      };
+    }
+  } catch (err) {
+    const error = new Error(err.message);
+    (error as any).status = 400;
+    throw error;
+  }
+
+  if (req.userId === Number(req.params.id)) {
+    profileDetail = {
+      ...profileDetail,
+      isOwner: true,
+    };
+  }
+
+  return profileDetail;
 };
