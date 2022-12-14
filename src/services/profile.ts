b8a9715679b33e4e24ac7710a79492a25121bef3
SHY <ekdmbzzang@naver.com>
2022-12-14 21:53:25 +0900

    [REFACT] Profile Detail View

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -61,100 +61,93 @@ export const view = async (req: Request) => {
   */
   // TODO : 평균응답시간, 좋아요, 채팅방 개수 추가
 
-  let profileDetail: profileDetailDto = {
-    isHost: false,
-    isOwner: false,
-    id: 0,
-    name: '',
-    username: '',
-    bio: '',
-    github: '',
-    stack: [],
-    email: '',
-    userId: 0,
-    isDeleted: false,
+  const getProfileGuest = async () => {
+    try {
+      const profileGuest = await AppDataSource.getRepository(ProfileGuest)
+        .createQueryBuilder('profile_guest')
+        .where('profile_guest.id = :id', { id: req.params.id })
+        .select([
+          'profile_guest.id',
+          'profile_guest.name',
+          'profile_guest.username',
+          'profile_guest.bio',
+          'profile_guest.image',
+          'profile_guest.userId',
+          'profile_guest.isDeleted',
+        ])
+        .getOneOrFail();
+      if (profileGuest.isDeleted) {
+        raiseError('This user is deleted', 404);
+      }
+      return profileGuest;
+    } catch (err) {
+      raiseError(err.message, 400);
+    }
+  };
+
+  const getEmailFromUser = async (userId: number) => {
+    try {
+      return await AppDataSource.getRepository(User)
+        .createQueryBuilder('user')
+        .where('user.userId = :id', { id: userId })
+        .select(['user.email'])
+        .getOneOrFail();
+    } catch (err) {
+      raiseError(err.message, 400);
+    }
+  };
+
+  const getProfileHostGithub = async (id: number) => {
+    return await AppDataSource.getRepository(ProfileHostGithub)
+      .createQueryBuilder('profile_host_github')
+      .where('profile_host_github.profile_guest_id = :id', { id: id })
+      .select(['profile_host_github.github'])
+      .getOne();
+  };
+
+  const getProfileHostTags = async (id: number) => {
+    return await AppDataSource.getRepository(ProfileHostTag)
+      .createQueryBuilder('profile_host_tag')
+      .where('profile_host_tag.profile_guest_id = :id', { id: id })
+      .select(['profile_host_tag.tag'])
+      .getMany();
+  };
+
+  const getIsOwner = async (req: Request, userId: number) => {
+    const token = req.header('access-token');
+    if (token) {
+      try {
+        const payload = jwt.verify(token, process.env.JWT_TOKEN_SECRET) as IPayload;
+        return payload.userId === Number(userId);
+      } catch (err) {
+        return false;
+      }
+    }
+  };
+
+  const raiseError = (msg, status) => {
+    const error = new Error(msg);
+    (error as any).status = status;
+    throw error;
   };
 
   try {
-    const profileGuestObj = await AppDataSource.getRepository(ProfileGuest)
-      .createQueryBuilder('profile_guest')
-      .where('profile_guest.id = :id', { id: req.params.id })
-      .select([
-        'profile_guest.id',
-        'profile_guest.name',
-        'profile_guest.username',
-        'profile_guest.bio',
-        'profile_guest.image',
-        'profile_guest.userId',
-        'profile_guest.isDeleted',
-      ])
-      .getOneOrFail();
-
-    if (profileGuestObj.isDeleted) {
-      const error = new Error('This user is deleted');
-      (error as any).status = 404;
-      throw error;
-    }
-
-    const email = await AppDataSource.getRepository(User)
-      .createQueryBuilder('user')
-      .where('user.userId = :id', { id: profileGuestObj.userId })
-      .select(['user.email'])
-      .getOneOrFail();
-
-    profileDetail = {
-      ...profileDetail,
-      ...profileGuestObj,
-      ...email,
-    };
-  } catch (err) {
-    const error = new Error(err.message);
-    (error as any).status = 404;
-    throw error;
-  }
-
-  try {
-    const profileHostGithub = await AppDataSource.getRepository(ProfileHostGithub)
-      .createQueryBuilder('profile_host_github')
-      .where('profile_host_github.profile_guest_id = :id', { id: profileDetail.id })
-      .select(['profile_host_github.github'])
-      .getOne();
-
+    const profileGuest = await getProfileGuest();
+    const email = await getEmailFromUser(profileGuest.userId);
+    const profileHostGithub = await getProfileHostGithub(profileGuest.id);
     const isHost = profileHostGithub !== null;
+    const profileHostTags = isHost ? await getProfileHostTags(profileGuest.id) : null;
+    const profileDetail: profileDetailDto = {
+      ...profileGuest,
+      ...email,
+      ...profileHostGithub,
+      stack: profileHostTags?.map((ProfileHostTag) => ProfileHostTag.tag),
+      isHost,
+      isOwner: await getIsOwner(req, profileGuest.userId),
+    };
 
-    if (isHost) {
-      const profileHostTags = await AppDataSource.getRepository(ProfileHostTag)
-        .createQueryBuilder('profile_host_tag')
-        .where('profile_host_tag.profile_guest_id = :id', { id: profileDetail.id })
-        .select(['profile_host_tag.tag'])
-        .getMany();
-
-      profileDetail = {
-        ...profileDetail,
-        ...profileHostGithub,
-        isHost,
-        stack: profileHostTags.map((ProfileHostTag) => ProfileHostTag.tag),
-      };
-    }
+    return profileDetail;
   } catch (err) {
-    const error = new Error(err.message);
-    (error as any).status = 400;
-    throw error;
+    raiseError(err.message, err.status);
   }
-
-  const token = req.header('access-token');
-  if (token) {
-    try {
-      const payload = jwt.verify(token, process.env.JWT_TOKEN_SECRET) as IPayload;
-
-      if (payload.userId === Number(profileDetail.userId)) {
-        profileDetail = {
-          ...profileDetail,
-          isOwner: true,
-        };
-      }
-    } catch (err) {}
-  }
-
-  return profileDetail;
 };
